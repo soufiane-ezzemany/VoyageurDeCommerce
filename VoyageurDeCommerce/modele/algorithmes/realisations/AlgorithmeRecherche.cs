da8401d7327f0e33fb50574b77695a3564b8fda2
Thomas AUTHIER <thomas.authier@iut-dijon.u-bourgogne.fr>
2022-05-20 16:21:49 +0200

    Modification de la recherche locale

--- VoyageurDeCommerce/modele/algorithmes/realisations/AlgorithmeRecherche.cs
+++ VoyageurDeCommerce/modele/algorithmes/realisations/AlgorithmeRecherche.cs
@@ -25,28 +25,38 @@ namespace VoyageurDeCommerce.modele.algorithmes.realisations
             this.Tournee = tourneeBase.Tournee;
             int min = this.Tournee.Distance;
 
-            //Permet de stocker toutes les tournées voisines obtenues
-            List<Tournee> tourneesVoisines = new List<Tournee>();
-            tourneesVoisines.Add(this.Tournee);
+            bool continuer = true;
 
-            //On parcourt les lieux de la tournée et on en inverse 2 dans une variable temporaire que l'on ajoute dans la liste
-            //des tournées voisines
-            for (int i=0; i < this.Tournee.ListeLieux.Count()-2; i++)
+            while (continuer == true)
             {
-                Tournee temp = new Tournee(this.Tournee);
-                temp.ListeLieux.Reverse(i, 2);
-                tourneesVoisines.Add(temp);
-            }
-            //On compare les tournées voisines avec la tournée qui a la plus petite distance
-            foreach (Tournee t in tourneesVoisines)
-            {
-                if (t.Distance <= min)
+                //Permet de stocker toutes les tournées voisines obtenues
+                List<Tournee> tourneesVoisines = new List<Tournee>();
+                tourneesVoisines.Add(this.Tournee);
+
+                //On parcourt les lieux de la tournée et on en inverse 2 dans une variable temporaire que l'on ajoute dans la liste
+                //des tournées voisines
+                for (int i = 0; i < this.Tournee.ListeLieux.Count() - 2; i++)
                 {
-                    min = t.Distance;
-                    this.Tournee = t;
+                    Tournee temp = new Tournee(this.Tournee);
+                    temp.ListeLieux.Reverse(i, 2);
+                    tourneesVoisines.Add(temp);
+                }
+                //On compare les tournées voisines avec la tournée qui a la plus petite distance
+                foreach (Tournee t in tourneesVoisines)
+                {
+                    if (t.Distance <= min)
+                    {
+                        min = t.Distance;
+                        this.Tournee = t;
+                        continuer = true;
+                    }
+                    else
+                    {
+                        continuer = false;
+                    }
                 }
             }
-
+            
             //Faire une copie de la tournée juste pour l'affichage constructif dans le programme
             List<Lieu> Lieux = new List<Lieu>(this.Tournee.ListeLieux);
             //Vider la tournée
